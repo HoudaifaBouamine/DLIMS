7a8c7a9e664395287285c470c17c2882125bc033
houdaifa bouamine <houdaifa.bouamine@gmail.com>
2023-11-22 20:29:30 +0100

    Create Database with Entity Framwork

--- DVLD.DataAccess/EntityFramworkDataLayer/Entities/Applications/ApplicationStatus.cs
+++ DVLD.DataAccess/EntityFramworkDataLayer/Entities/Applications/ApplicationStatus.cs
@@ -7,13 +7,13 @@ using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DVLD.DataAccess.EntityFramworkDataLayer.Entities
+namespace DVLD.DataAccess.EntityFramworkDataLayer.Entities.Applications
 {
     public class ApplicationStatus
     {
         [Key]
         [Column("ApplicationStatus_Id")]
-        public int ApplicationStatus_Id {  get; set; }
+        public int ApplicationStatus_Id { get; set; }
 
         [MaxLength(20)]
         public string Name { get; set; } = string.Empty;
--- DVLD.DataAccess/EntityFramworkDataLayer/Entities/License/LicenseClass.cs
+++ DVLD.DataAccess/EntityFramworkDataLayer/Entities/License/LicenseClass.cs
@@ -13,8 +13,8 @@ namespace DVLD.DataAccess.EntityFramworkDataLayer.Entities.License
     {
 
         [Key]
-
-        int LicenseClass_Id;
+        [Column("LicenseClass_Id")]
+        public int LicenseClass_Id { get; set; }
 
         [Required] public string Name { get; set; } = string.Empty;
         [Required] public string Description { get; set; } = string.Empty;
